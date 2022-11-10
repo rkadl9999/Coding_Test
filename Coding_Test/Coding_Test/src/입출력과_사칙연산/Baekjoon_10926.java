2de26a01614c8ba9c9ac98f246ed67b9c84113c5
rkadl9999 <rkadl9999@naver.com>
2022-11-10 13:54:08 +0900

    ✨| 10926번 문제

---
+++ Coding_Test/Coding_Test/src/입출력과_사칙연산/Baekjoon_10926.java
@@ -0,0 +1,13 @@
+package 입출력과_사칙연산;
+
+import java.util.Scanner;
+
+public class Baekjoon_10926 {
+    public static void main(String[] argc){
+        Scanner sc = new Scanner(System.in);
+
+        String id = sc.next();
+
+        System.out.println(id+"??!");
+    }
+}
