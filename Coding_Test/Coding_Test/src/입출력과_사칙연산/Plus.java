d1dea8ab8897733c3e21784ea742406a9f8adf6a
rkadl9999 <rkadl9999@naver.com>
2022-11-10 10:30:58 +0900

    ✨| A+B

---
+++ Coding_Test/Coding_Test/src/입출력과_사칙연산/Plus.java
@@ -0,0 +1,14 @@
+package 입출력과_사칙연산;
+
+import java.util.Scanner;
+
+public class Plus {
+    public static void main(String[] argc) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int result = A + B;
+
+        System.out.println(result);
+    }
+}
