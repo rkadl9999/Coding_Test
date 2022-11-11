01238f53afece40366918dfb19f7fc4e87eaabdf
rkadl9999 <rkadl9999@naver.com>
2022-11-11 14:23:05 +0900

    ✨| 야옹이 출력

---
+++ Coding_Test/Coding_Test/src/입출력과_사칙연산/Cat_Print.java
@@ -0,0 +1,10 @@
+package 입출력과_사칙연산;
+
+public class Cat_Print {
+    public static void main(String[] argc){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
