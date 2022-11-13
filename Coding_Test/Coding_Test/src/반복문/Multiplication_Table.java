de097d59e6a448c24f14050fd4ae6a082997804f
rkadl9999 <rkadl9999@naver.com>
2022-11-13 15:24:51 +0900

    ✨| 구구단

---
+++ Coding_Test/Coding_Test/src/반복문/Multiplication_Table.java
@@ -0,0 +1,14 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class Multiplication_Table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=1; i<=9; i++)
+            System.out.println(n + " * " + i + " = " + n*i);
+    }
+}
