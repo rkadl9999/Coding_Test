be87fa195d73491f9a939aede4fa0b200cd93cbf
rkadl9999 <rkadl9999@naver.com>
2022-11-13 11:54:05 +0900

    ✨| 오븐 시계

---
+++ Coding_Test/Coding_Test/src/조건문/Oven_Clock.java
@@ -0,0 +1,27 @@
+package 조건문;
+
+import java.util.Scanner;
+
+public class Oven_Clock {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int hour = sc.nextInt();
+        int min = sc.nextInt();
+        int time = sc.nextInt();
+
+        if(min + time >= 60){
+            hour += (min + time) / 60;
+            if(hour >= 24)
+                hour -= 24;
+            min = (min + time) % 60;
+            System.out.println(min);
+            if(min >= 60)
+                min -= 60;
+        }
+        else if(min + time < 60)
+            min += time;
+
+        System.out.println(hour + " " + min);
+    }
+}
